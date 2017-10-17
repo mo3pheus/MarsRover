ac3f0d7f7d84e91e73d86068d69ca94c925edf57
Sanket <sanket.korgaonkar@gmail.com>
2017-10-16 21:06:48 -0500

    Feature: Weather data parsing completed.

--- mars.rover/src/main/java/space/exploration/mars/rover/dataUplink/WeatherQueryService.java
+++ mars.rover/src/main/java/space/exploration/mars/rover/dataUplink/WeatherQueryService.java
@@ -1,5 +1,9 @@
 package space.exploration.mars.rover.dataUplink;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import space.exploration.mars.rover.utils.WeatherUtil;
+
 public class WeatherQueryService extends QueryService {
 
     @Override
@@ -9,32 +13,32 @@ public class WeatherQueryService extends QueryService {
 
     @Override
     public Object getResponse() {
-        WeatherData.WeatherPayload.Builder wBuilder = WeatherData.WeatherPayload.newBuilder();
-
-        return wBuilder.build();
+        Document                   document       = Jsoup.parse(getResponseAsString());
+        WeatherData.WeatherPayload weatherPayload = WeatherUtil.getweatherPayload(document.body().text());
+        return weatherPayload;
     }
 }
 
 
 /**
-
- {
- "report": {
- "terrestrial_date": "2017-10-10",
- "sol": 1841,
- "ls": 72.0,
- "min_temp": -77.0,
- "min_temp_fahrenheit": -106.6,
- "max_temp": -32.0,
- "max_temp_fahrenheit": -25.6,
- "pressure": 870.0,
- "pressure_string": "Higher",
- "abs_humidity": null,
- "wind_speed": null,
- "wind_direction": "--",
- "atmo_opacity": "Sunny",
- "season": "Month 3",
- "sunrise": "2017-10-10T11:00:00Z",
- "sunset": "2017-10-10T22:43:00Z"
- }
- }**/
+ * {
+ * "report": {
+ * "terrestrial_date": "2017-10-10",
+ * "sol": 1841,
+ * "ls": 72.0,
+ * "min_temp": -77.0,
+ * "min_temp_fahrenheit": -106.6,
+ * "max_temp": -32.0,
+ * "max_temp_fahrenheit": -25.6,
+ * "pressure": 870.0,
+ * "pressure_string": "Higher",
+ * "abs_humidity": null,
+ * "wind_speed": null,
+ * "wind_direction": "--",
+ * "atmo_opacity": "Sunny",
+ * "season": "Month 3",
+ * "sunrise": "2017-10-10T11:00:00Z",
+ * "sunset": "2017-10-10T22:43:00Z"
+ * }
+ * }
+ **/
--- mars.rover/src/main/java/space/exploration/mars/rover/utils/WeatherUtil.java
+++ mars.rover/src/main/java/space/exploration/mars/rover/utils/WeatherUtil.java
@@ -1,8 +1,11 @@
 package space.exploration.mars.rover.utils;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import space.exploration.mars.rover.dataUplink.WeatherData;
 
 public class WeatherUtil {
+    private static Logger logger = LoggerFactory.getLogger(WeatherUtil.class);
 
     private static final String[] WEATHER_ATTRS = {"\"terrestrial_date\":",
                                                    "\"sol\":",
@@ -23,16 +26,86 @@ public class WeatherUtil {
                                                    };
 
     public static WeatherData.WeatherPayload getweatherPayload(String weatherData) {
+        WeatherData.WeatherPayload.Builder wBuilder = WeatherData.WeatherPayload.newBuilder();
+        wBuilder.setAbsoluteHumidity(getValue(weatherData, "\"abs_humidity\""));
+        wBuilder.setAtmosphericOpacity(getValue(weatherData, "\"atmo_opacity"));
 
+        try {
+            int sol = Integer.parseInt(getValue(weatherData, "\"sol\":"));
+            wBuilder.setSol(sol);
+        } catch (NumberFormatException nfe) {
+            logger.info("Sol information not present", nfe);
+        }
 
-        return null;
+        try {
+            double ls = Double.parseDouble(getValue(weatherData, "\"ls\":"));
+            wBuilder.setLs(ls);
+        } catch (NumberFormatException nfe) {
+            logger.info("ls information not present", nfe);
+        }
+
+        try {
+            double minTemp = Double.parseDouble(getValue(weatherData, "\"min_temp\":"));
+            wBuilder.setMinTemp(minTemp);
+        } catch (NumberFormatException nfe) {
+            logger.info("minTemp information not present", nfe);
+        }
+
+        try {
+            wBuilder.setMaxTemp(Double.parseDouble(getValue(weatherData, "\"max_temp\":")));
+        } catch (NumberFormatException nfe) {
+            logger.info("maxTemp information not present", nfe);
+        }
+
+        try {
+            wBuilder.setMinTemFahrenheit(Double.parseDouble(getValue(weatherData, "\"min_temp_fahrenheit\":")));
+        } catch (NumberFormatException nfe) {
+            logger.info("minTempFahrenheit information not present", nfe);
+        }
+
+        try {
+            wBuilder.setMaxTempFahrenheit(Double.parseDouble(getValue(weatherData, "\"max_temp_fahrenheit\":")));
+        } catch (NumberFormatException nfe) {
+            logger.info("maxTempFahrenheit information not present", nfe);
+        }
+
+        try{
+            wBuilder.setWindSpeed(Double.parseDouble(getValue(weatherData, "\"wind_speed\":")));
+        } catch (NumberFormatException nfe) {
+            logger.info("wind_speed information not present", nfe);
+        }
+
+        try{
+            wBuilder.setWindDirection(Double.parseDouble(getValue(weatherData, "\"wind_direction\":")));
+        } catch (NumberFormatException nfe) {
+            logger.info("wind_direction information not present", nfe);
+        }
+
+        wBuilder.setPressureDescription(getValue(weatherData, "\"pressure_string\":"));
+        wBuilder.setSeason(getValue(weatherData, "\"season\":"));
+        wBuilder.setSunrise(getValue(weatherData, "\"sunrise\":"));
+        wBuilder.setSunset(getValue(weatherData, "\"sunset\":"));
+
+        return wBuilder.build();
     }
 
     public static String getValue(String body, String tag) {
         int    startPosn = body.indexOf(tag);
         String valString = body.substring(startPosn);
         int    endPosn   = valString.indexOf(",");
-        return body.substring(startPosn, startPosn + endPosn);
+
+        if((endPosn == -1)&&(tag.equals("\"sunset\":"))){
+            endPosn = valString.lastIndexOf("}");
+        }
+
+        String relevantText = body.substring(startPosn, startPosn + endPosn);
+        relevantText = relevantText.replaceAll(tag, "");
+        relevantText = relevantText.replaceAll("\"", "");
+        relevantText = relevantText.replaceAll(":", "");
+        relevantText = relevantText.replaceAll(" ", "");
+        relevantText = relevantText.replaceAll("}","");
+        relevantText = relevantText.replaceAll("\\{","");
+        return relevantText;
     }
 
     private static Object extractValue(String body, String tag) {
