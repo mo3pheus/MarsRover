8d0d58ee4bf4b50e590c7bf3b33b84127f1425e0
Sanket <sanket.korgaonkar@gmail.com>
2018-02-25 22:53:19 -0600

    Datadump

--- mars.rover/deployment/deploy.sh
+++ mars.rover/deployment/deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+# import time
 echo "Deploying Mars Rover"
 pwd=$(echo pwd)
 dataArchiveLocation=$($pwd)/dataArchives
-java -jar target/mars.rover-1.5-SOLSHOT-shaded.jar src/main/resources/marsConfig.properties src/main/resources/roverDB.properties src/main/resources/ $dataArchiveLocation $1
-
+nohup java -jar target/mars.rover-1.5-SOLSHOT-shaded.jar src/main/resources/marsConfig.properties src/main/resources/roverDB.properties src/main/resources/ $dataArchiveLocation $1 &
+# gnome-terminal --command="./deployment/tailRoverLogs.sh"
+sleep 5
+./deployment/tailRoverLogs.sh
