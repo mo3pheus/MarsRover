a1957fdc8028b11f4c55bf41c1d8a222e7f463d6
Sanket <sanket.korgaonkar@gmail.com>
2018-11-08 21:51:46 -0500

    Feature: temp work - still havent found valid ranges.
    Do not check in - temp work.

--- mars.rover/src/test/java/space/exploration/mars/rover/kernel/PositionSensorDataUtil.java
+++ mars.rover/src/test/java/space/exploration/mars/rover/kernel/PositionSensorDataUtil.java
@@ -8,40 +8,111 @@ import org.slf4j.LoggerFactory;
 import space.exploration.communications.protocol.spice.MSLRelativePositions;
 import space.exploration.spice.utilities.PositionUtils;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class PositionSensorDataUtil {
-    private PositionUtils                                    positionUtils    = null;
-    private DateTime                                         startTime        = null;
-    private DateTime                                         endTime          = null;
-    private Logger                                           logger           = LoggerFactory.getLogger(PositionSensorDataUtil.class);
-    private List<MSLRelativePositions.MSLRelPositionsPacket> positionsPackets = new ArrayList<>();
+    private final int           MAX_ATTEMPTS          = 1000;
+    private       PositionUtils positionUtils         = null;
+    private       DateTime      startTime             = null;
+    private       DateTime      endTime               = null;
+    private       Logger        logger                = LoggerFactory
+            .getLogger(PositionSensorDataUtil.class);
+    private       List<Long>    coverageGaps          = new ArrayList<>();
+    private       List<String>  formattedCoverageGaps = new ArrayList<>();
+
+    DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd~HH:mm:ss");
 
     public PositionSensorDataUtil(Properties roverConfig) {
         positionUtils = new PositionUtils(roverConfig.getProperty("mars.rover.spice.positionUtilFile"));
     }
 
-    public void getCoverageData(String startDate, String endDate) {
-        DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd~HH:mm:ss");
+    public List<Long> getCoverageData(String startDate, String endDate) {
         startTime = formatter.parseDateTime(startDate);
         endTime = formatter.parseDateTime(endDate);
 
-        while ( startTime.getMillis() < endTime.getMillis() ) {
+        while (startTime.getMillis() < endTime.getMillis()) {
             positionUtils.setUtcTime(formatter.print(startTime));
             try {
                 MSLRelativePositions.MSLRelPositionsPacket positionsPacket = positionUtils.getPositionPacket();
-                logger.info("Positions Packet at " + formatter.print(startTime) + " => " + positionsPacket.toString());
-                positionsPackets.add(positionsPacket);
-                //Thread.sleep(1000l);
             } catch (Exception e) {
                 logger.error("Coverage gap at " + formatter.print(startTime));
+                formattedCoverageGaps.add(formatter.print(startTime.getMillis()));
+                coverageGaps.add(startTime.getMillis());
             }
-            //startTime = new DateTime(startTime.getMillis() + TimeUnit.HOURS.toMillis(1l));
             startTime = new DateTime(startTime.getMillis() + TimeUnit.MINUTES.toMillis(10l));
         }
 
+        return coverageGaps;
+    }
+
+    public List<String> getFormattedCoverageGaps() {
+        return formattedCoverageGaps;
+    }
+
+    protected MSLRelativePositions.MSLRelPositionsPacket[] getNearestValidData(Long timestamp) throws IllegalArgumentException {
+        MSLRelativePositions.MSLRelPositionsPacket[] rangingData = new MSLRelativePositions.MSLRelPositionsPacket[150];
+
+        List<Long>                                            timestampList      = new ArrayList<>();
+        Map<Long, MSLRelativePositions.MSLRelPositionsPacket> positionsPacketMap = new HashMap<>();
+
+        int i                  = 0;
+        int preTimestampCount  = 0;
+        int postTimestampCount = 0;
+        int numAttempts        = 0;
+
+        Long currentTimestamp = timestamp - TimeUnit.MINUTES.toMillis(1l);
+
+        while (preTimestampCount < 100) {
+            if (numAttempts++ > MAX_ATTEMPTS) {
+                throw new IllegalArgumentException("Can not find valid range for timestamp = " + timestamp);
+            }
+
+            try {
+                positionUtils.setUtcTime(formatter.print(currentTimestamp));
+                MSLRelativePositions.MSLRelPositionsPacket positionsPacket = positionUtils.getPositionPacket();
+                timestampList.add(currentTimestamp);
+                positionsPacketMap.put(currentTimestamp, positionsPacket);
+                preTimestampCount++;
+            } catch (Exception e) {
+                logger.debug("Gap at timestamp = " + currentTimestamp, e);
+                logger.info(
+                        "NumAttempts = " + numAttempts + " validCount so far = " + preTimestampCount + " currentTS = "
+                                + formatter
+                                .print(currentTimestamp));
+            }
+            currentTimestamp -= TimeUnit.MINUTES.toMillis(1l);
+        }
+
+        currentTimestamp = timestamp + TimeUnit.MINUTES.toMillis(1l);
+        while (postTimestampCount < 50) {
+            if (numAttempts++ > MAX_ATTEMPTS) {
+                throw new IllegalArgumentException("Can not find valid range for timestamp = " + timestamp);
+            }
+
+            try {
+                positionUtils.setUtcTime(formatter.print(currentTimestamp));
+                MSLRelativePositions.MSLRelPositionsPacket positionsPacket = positionUtils.getPositionPacket();
+                timestampList.add(currentTimestamp);
+                positionsPacketMap.put(currentTimestamp, positionsPacket);
+                preTimestampCount++;
+            } catch (Exception e) {
+                logger.debug("Gap at timestamp = " + currentTimestamp, e);
+                logger.info(
+                        "NumAttempts = " + numAttempts + " validCount so far = " + preTimestampCount + " currentTS = "
+                                + formatter
+                                .print(currentTimestamp));
+            }
+            currentTimestamp += TimeUnit.MINUTES.toMillis(1l);
+        }
+
+        Collections.sort(timestampList);
+        int index = 0;
+        while (index < rangingData.length) {
+            rangingData[index] = positionsPacketMap.get(timestampList.get(index));
+            index++;
+        }
+
+        return rangingData;
     }
 }
--- mars.rover/src/test/java/space/exploration/mars/rover/kernel/PositionTestDriver.java
+++ mars.rover/src/test/java/space/exploration/mars/rover/kernel/PositionTestDriver.java
@@ -1,16 +1,37 @@
 package space.exploration.mars.rover.kernel;
 
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import space.exploration.communications.protocol.spice.MSLRelativePositions;
 import space.exploration.mars.rover.bootstrap.MarsMissionLaunch;
 import space.exploration.mars.rover.bootstrap.MatrixCreation;
 
+import java.util.List;
 import java.util.Properties;
 
 public class PositionTestDriver {
+
+    static Logger logger = LoggerFactory.getLogger(PositionTestDriver.class);
 
     public static void main(String[] args) throws Exception {
         MarsMissionLaunch.configureConsoleLogging(false);
         Properties             roverConfig            = MatrixCreation.getConfig();
         PositionSensorDataUtil positionSensorDataUtil = new PositionSensorDataUtil(roverConfig);
-        positionSensorDataUtil.getCoverageData("2016-03-17~11:00:00", "2016-03-17~13:00:00");
+        List<Long> coverageGaps = positionSensorDataUtil
+                .getCoverageData("2016-03-17~11:00:00", "2016-03-17~13:00:00");
+        for (String timestamp : positionSensorDataUtil.getFormattedCoverageGaps()) {
+            logger.info("Coverage gap at " + timestamp);
+        }
+
+        for (Long timestamp : coverageGaps) {
+            MSLRelativePositions.MSLRelPositionsPacket[] rangingData = positionSensorDataUtil
+                    .getNearestValidData(timestamp);
+            logger.info("Coverage gap timestamp = " + timestamp + " validRange pulledout = " + rangingData.length);
+            logger.info("***************************************************");
+            for (MSLRelativePositions.MSLRelPositionsPacket positionsPacket : rangingData) {
+                logger.info(positionsPacket.toString());
+            }
+        }
     }
 }
