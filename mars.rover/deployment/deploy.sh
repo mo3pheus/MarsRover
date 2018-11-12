f3daabdf8ac8655903a334bce124c79391dfb103
Sanket <sanket.korgaonkar@gmail.com>
2018-11-12 00:18:27 -0500

    Bugfix: Fixed deployment script.

--- mars.rover/deployment/deploy.sh
+++ mars.rover/deployment/deploy.sh
@@ -3,7 +3,7 @@
 echo "Deploying Mars Rover"
 pwd=$(echo pwd)
 dataArchiveLocation=$($pwd)/dataArchives
-nohup java -jar target/mars.rover-2.1-SOLSHOT-shaded.jar src/main/resources/marsConfig.properties src/main/resources/roverDB.properties src/main/resources/ $dataArchiveLocation $1 zion-portable &
+nohup java -jar target/mars.rover-2.1-SOLSHOT-shaded.jar src/main/resources/marsConfig.properties src/main/resources/roverDB.properties src/main/resources/ $dataArchiveLocation antares-mainframe $1 &
 # gnome-terminal --command="./deployment/tailRoverLogs.sh"
 sleep 5
 ./deployment/tailRoverLogs.sh
